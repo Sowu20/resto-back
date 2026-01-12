be13ddde73a5166882a1a96031e4a931a25bdb65
Sowu20 <sowukelly67@gmail.com>
2026-01-12 10:41:48 +0000

    commit

--- app.js
+++ app.js
@@ -8,19 +8,12 @@ const app = express();
 
 app.use(express.json()); 
 
-// app.use(cors({
-//     origin: 'https://resto-back-xazy.onrender.com',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors({
+    origin: 'https://resto-back-xazy.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 // Connexion à MongoDB via Mongoose
 mongoose.connect("mongodb+srv://sowukelly67:sowukelly@clusterresto.srmvdzb.mongodb.net/ClusterResto?appName=ClusterResto")
