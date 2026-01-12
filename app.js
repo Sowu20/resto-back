76edab97a732821e22893acb3738f85bc39eaee0
Sowu20 <sowukelly67@gmail.com>
2026-01-12 09:45:56 +0000

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json()); 
 
 app.use(cors({
-    origin: 'https://resto-back-xnxb.onrender.com, *',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
