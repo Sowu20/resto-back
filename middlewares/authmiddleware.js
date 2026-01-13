b72bd4b989bb87a39d802fff5462ca25a4099112
Sowu20 <sowukelly67@gmail.com>
2026-01-13 11:14:52 +0000

    Correction du middleware d'authentification

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     req.user = {
       id: decoded.id,
