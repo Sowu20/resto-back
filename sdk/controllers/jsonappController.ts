bb055c4f5f6ba28d91ea967d8224946d0f11bc9d
Sowu20 <sowukelly67@gmail.com>
2026-01-15 13:10:44 +0000

    Correction des endpoint

--- sdk/controllers/jsonappController.ts
+++ sdk/controllers/jsonappController.ts
@@ -2,47 +2,14 @@ import { mainMenu } from '../views/menuView/mainMenu';
 import { registerForm } from '../views/formView/registerForm';
 import { userReader } from '../views/readerView/Reader';
 
-const formViews: Record<string, any> = {
-    'registerForm': registerForm,
-};
-
-const readerViews: Record<string, any> = {
-    'userReader': userReader,
-};
-
-const actionViews: Record<string, any> = {
-    'mainMenu': mainMenu,
-};
-
 export const getMenu = (req: any, res: any) => {
-    const { viewId } = req.params;
-    const menu = actionViews[viewId];
-    if (!menu) {
-        return res.status(404).json({
-            message: 'Menu non trouvé'
-        });
-    }
-    res.json(menu.toJSON());
+    res.json(mainMenu.toJSON());
 };
 
 export const getForm = (req: any, res: any) => {
-    const { formId } = req.params;
-    const form = formViews[formId];
-    if (!form) {
-        return res.status(404).json({
-            message: 'Formulaire non trouvé'
-        })
-    }
-    res.json(form.toJSON());
+    res.json(registerForm.toJSON());
 };
 
 export const getReader = (req: any, res: any) => {
-    const { readerId } = req.params;
-    const reader = readerViews[readerId];
-    if (!reader) {
-        return res.status(404).json({
-            message: 'Lecteur non trouvé'
-        })
-    }
-    res.json(reader.toJSON());
+    res.json(userReader.toJSON());
 };
--- sdk/routes/jsonappRoute.ts
+++ sdk/routes/jsonappRoute.ts
@@ -3,8 +3,8 @@ import { getForm, getMenu, getReader } from "../controllers/jsonappController";
 
 const router = express.Router();
 
-router.get('/actions/:viewId', getMenu);
-router.get('/forms/:formId', getForm);
-router.get('/readers/:readerId', getReader);
+router.get('/action', getMenu);
+router.get('/forms', getForm);
+router.get('/readers', getReader);
 
 export default router;
