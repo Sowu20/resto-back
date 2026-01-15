648832c97bda59013faa873e6b0c58a5c4203f69
Sowu20 <sowukelly67@gmail.com>
2026-01-15 15:40:21 +0000

    Modification du champ role du modèle User

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['Restaurateur', 'Admin'],
+        enum: ['restaurent', 'Admin'],
         required: true
     },
     password: {
