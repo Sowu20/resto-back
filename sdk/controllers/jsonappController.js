8334082a7ed065f7e8b1b2b4a35bfb6c14dc94ab
Sowu20 <sowukelly67@gmail.com>
2026-01-19 15:29:21 +0000

    Modification et ajout des routes dans le sdk

--- sdk/controllers/jsonappController.js
+++ sdk/controllers/jsonappController.js
@@ -1,6 +1,8 @@
 const { mainMenu } = require('../views/menuView/mainMenu');
 const { registerForm } = require('../views/formView/registerForm');
 const { userReader } = require('../views/readerView/reader');
+const { listrestaurentReader } = require('../views/readerView/listrestaurentReader');
+const { restaurentReader } = require('../views/readerView/restaurentReader');
 
 const getMenu = (req, res) => {
     res.json(mainMenu.toJSON());
@@ -14,4 +16,12 @@ const getReader = (req, res) => {
     res.json(userReader.toJSON());
 };
 
-module.exports = { getMenu, getForm, getReader };
+const getlistRestaurent = (req, res) => {
+    res.json(listrestaurentReader.toJSON());
+};
+
+const getRestaurent = (req, res) => {
+    res.json(restaurentReader.toJSON());
+};
+
+module.exports = { getMenu, getForm, getReader, getlistRestaurent, getRestaurent };
--- sdk/routes/jsonappRoute.js
+++ sdk/routes/jsonappRoute.js
@@ -1,10 +1,12 @@
 const express = require('express'); 
-const { getForm, getMenu, getReader } = require('../controllers/jsonappController');
+const { getForm, getMenu, getReader, getlistRestaurent, getRestaurent } = require('../controllers/jsonappController');
 
 const router = express.Router();
 
 router.get('/action', getMenu);
 router.get('/forms', getForm);
 router.get('/readers', getReader);
+router.get('/listrestaurent', getlistRestaurent);
+router.get('/restaurent', getRestaurent);
 
 module.exports = router;
