694672650124818dae45356c500cffdea152fbf0
Sowu20 <sowukelly67@gmail.com>
2026-01-20 16:06:53 +0000

    commit

--- sdk/views/readerView/listrestaurentReader.js
+++ sdk/views/readerView/listrestaurentReader.js
@@ -3,6 +3,6 @@ const { ReaderView } = require('@numerum-tech/cmsdk');
 const listrestaurentReader = new ReaderView('restaurent-list', 'Liste des restaurets')
     .setIntro('Sélectionner un restaurent pour consulter son menu')
     .addParagraph('Liste des restaurents')
-    .addLink('ChezNana', '/1');
+    .addLink('/1', 'ChezNana');
 
 module.exports = { listrestaurentReader }
