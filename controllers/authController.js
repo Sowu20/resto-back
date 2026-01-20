b40e828c6ec07dad96ff74b8a905d05b21a002d5
Sowu20 <sowukelly67@gmail.com>
2026-01-20 09:58:18 +0000

    Corection de requête d'inscription et de connexion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const Restaurent = require('../models/Restaurent');
 
 const secret_key ='0a01fcf7e3084f800565d0bd2d9a69de63b043d3c91274306a40b3df8c03ed05';
 
@@ -42,6 +43,52 @@ exports.register = async(req, res) => {
     }
 };
 
+exports.registerRestaurent = async(req, res) => {
+    try {
+        const { name, address, phone, email, password } = req.body;
+
+        const exist = await User.findOne({ email });
+        if (exist) {
+            return res.status(400).json({
+                message: "Email déjà utilisé"
+            });
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            address: address,
+            phone,
+            email,
+            role: 'Restaurateur',
+            password: hashedPass
+        });
+
+        const restaurent = await Restaurent.create({
+            name: name,
+            address,
+            phone,
+            email,
+            user: user._id
+        });
+
+        return res.status(201).json({
+            message: "Restaurent créé avec succès",
+            restaurent,
+            user: {
+                id: user._id,
+                email: user.email,
+                role: user.role
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Erreur serveur"
+        });
+    }
+};
+
 exports.login = async(req, res) => {
     try {
         const { email, password } = req.body;
@@ -52,6 +99,8 @@ exports.login = async(req, res) => {
                 message: 'Utilisateur non trouvé'
             });
         }
+
+        const restaurent = await Restaurent.findOne({ user: user._id });
 
         // Comparer le mot de passe
         const validPass = await bcrypt.compare(password, user.password);
@@ -66,7 +115,7 @@ exports.login = async(req, res) => {
             { 
                 id: user._id, 
                 role: user.role,
-                restaurentId: user.restaurent || null
+                restaurentId: restaurent ? restaurent._id: null
             },
             secret_key || 'default_secret',
             { expiresIn: "2d" }  
