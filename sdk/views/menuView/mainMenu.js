1664112e1ddec099d0cb92911eb253f44f059e08
Sowu20 <sowukelly67@gmail.com>
2026-01-20 14:52:13 +0000

    m

--- sdk/views/menuView/mainMenu.js
+++ sdk/views/menuView/mainMenu.js
@@ -1,6 +1,6 @@
 const { ActionListView } = require('@numerum-tech/cmsdk');
 
 const mainMenu = new ActionListView('main-menu', 'Acceuil')
-    .addAction('restaurents', 'Restaurents', 'Consulter la liste des restaurents disponible', { href: '/listrestaurent' });
+    .addAction('restaurents', 'Restaurents', 'Consulter la liste des restaurents disponible', '/listrestaurent');
 
 module.exports = { mainMenu };
