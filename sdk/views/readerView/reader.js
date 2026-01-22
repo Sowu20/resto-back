440e314215a1f75bb2557e90f42d3fba392afa52
Julio DAKLU <sergiodaklu12@gmail.com>
2026-01-22 12:36:26 +0000

    correction des routes

--- sdk/views/readerView/reader.js
+++ sdk/views/readerView/reader.js
@@ -1,15 +1,9 @@
 const { ReaderView } = require('@numerum-tech/cmsdk');
 
-const userReader = new ReaderView('user-reader', "Détail d'un utilisateur")
-    .setIntro('Détail du compte')
-    .addListField([
-        'Name: Amad',
-        'Adress: Agoè',
-        'Phone: 87908765',
-        'Email: amad@gmail.com',
-        'Role: Admin',
-    ])
-    .addSeparator()
-    .addParagraph('Compte actif et validé');
+const userReader = new ReaderView('user-profile', 'Mon Profil')
+    .setIntro('Bienvenue sur votre profil')
+    .addParagraph('Nom: John Doe')
+    .addParagraph('Email: john@example.com')
+    .addLink('/mobile/forms', 'Modifier le profil');
 
 module.exports = { userReader };
