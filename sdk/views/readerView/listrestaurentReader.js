aa18596dfa2298d544da9e517deef79d22168530
Sowu20 <sowukelly67@gmail.com>
2026-01-22 11:44:57 +0000

    Modification de la liste des restaurents avec ActionGridView

--- sdk/views/readerView/listrestaurentReader.js
+++ sdk/views/readerView/listrestaurentReader.js
@@ -1,11 +1,11 @@
-const { ActionListView } = require('@numerum-tech/cmsdk');
+// const { ActionListView } = require('@numerum-tech/cmsdk');
 
-const listrestaurentMenu = new ActionListView('restaurant-list', 'Nos Restaurants')
-    .addAction('1', 'Chez Nana', 'Restaurant africain traditionnel')
-    .addAction('2', 'Chez Bordille', 'Spécialités locales et grillades')
-    .addAction('3', 'Chez Manon', 'Cuisine raffinée et poissons frais');
+// const listrestaurentMenu = new ActionListView('restaurant-list', 'Nos Restaurants')
+//     .addAction('1', 'Chez Nana', 'Restaurant africain traditionnel')
+//     .addAction('2', 'Chez Bordille', 'Spécialités locales et grillades')
+//     .addAction('3', 'Chez Manon', 'Cuisine raffinée et poissons frais');
 
-module.exports = { listrestaurentMenu };
+// module.exports = { listrestaurentMenu };
 
 
 // const { ReaderView } = require('@numerum-tech/cmsdk');
@@ -17,3 +17,14 @@ module.exports = { listrestaurentMenu };
 //     .addLink('/3', 'Chez Manon');
 
 // module.exports = { listrestaurentReader }
+
+const { ActionGridView } = require('@numerum-tech/cmsdk');
+
+const listrestaurentMenu = new ActionGridView('restaurent-list', 'Nos restaurents')
+    .setColumns(3)
+    .setSpacing(18)
+    .addAction('1', 'Chez Nana', 'Restaurant africain traditionnel', 'https://resto-back-xazy.onrender.com/assets/resto1.jpg')
+    .addAction('2', 'Chez Bordille', 'Spécialités locales et grillades', 'https://resto-back-xazy.onrender.com/assets/resto2.jpg')
+    .addAction('3', 'Chez Manon', 'Cuisine raffinée et poissons frais', 'https://resto-back-xazy.onrender.com/assets/resto3.jpg');
+
+module.exports = { listrestaurentMenu }
