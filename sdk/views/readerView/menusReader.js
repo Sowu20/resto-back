006e24d0b734e1bddebde9898414f94d9566c3c8
Sowu20 <sowukelly67@gmail.com>
2026-01-25 18:25:17 +0000

    Ajout des images des menus

--- sdk/views/readerView/menusReader.js
+++ sdk/views/readerView/menusReader.js
@@ -5,26 +5,26 @@ const menusData = {
   '1': {
     nom: 'Chez Nana',
     menu: [
-      { id: 'petit-dej-1', nom: 'Menu Petit-déjeuner', prix: '2000 FCFA', description: 'Café, jus, croissants, fruits' },
-      { id: 'dej-1', nom: 'Menu Déjeuner', prix: '3500 FCFA', description: 'Plat du jour, dessert, boisson' },
-      { id: 'diner-1', nom: 'Menu Dîner', prix: '5000 FCFA', description: 'Entrée, plat, dessert, vin' }
+      { id: 'petit-dej-1', nom: 'Menu Petit-déjeuner', prix: '2000 FCFA', description: 'Café, jus, croissants, fruits', image: 'https://resto-back-xazy.onrender.com/assets/petitdej.jpg' },
+      { id: 'dej-1', nom: 'Menu Déjeuner', prix: '3500 FCFA', description: 'Plat du jour, dessert, boisson', image: 'https://resto-back-xazy.onrender.com/assets/dej1.jpg' },
+      { id: 'diner-1', nom: 'Menu Dîner', prix: '5000 FCFA', description: 'Entrée, plat, dessert, vin', image: 'https://resto-back-xazy.onrender.com/assets/midi.jpg' }
     ]
   },
   '2': {
     nom: 'Chez Bordille',
     menu: [
-      { id: 'brunch-2', nom: 'Menu Brunch', prix: '2500 FCFA', description: 'Oeufs, bacon, pancakes, café' },
-      { id: 'midi-2', nom: 'Menu Midi', prix: '4000 FCFA', description: 'Salade, plat chaud, boisson' },
-      { id: 'soir-2', nom: 'Menu Soir', prix: '6000 FCFA', description: 'Menu complet gastronomique' },
-      { id: 'famille-2', nom: 'Menu Famille', prix: '15000 FCFA', description: 'Pour 4 personnes - variété de plats' }
+      { id: 'brunch-2', nom: 'Menu Brunch', prix: '2500 FCFA', description: 'Oeufs, bacon, pancakes, café', image: 'https://resto-back-xazy.onrender.com/assets/brunch.jpg' },
+      { id: 'midi-2', nom: 'Menu Midi', prix: '4000 FCFA', description: 'Salade, plat chaud, boisson', image: 'https://resto-back-xazy.onrender.com/assets/midi.jpg' },
+      { id: 'soir-2', nom: 'Menu Soir', prix: '6000 FCFA', description: 'Menu complet gastronomique', image: 'https://resto-back-xazy.onrender.com/assets/soir.jpg' },
+      { id: 'famille-2', nom: 'Menu Famille', prix: '15000 FCFA', description: 'Pour 4 personnes - variété de plats', image: 'https://resto-back-xazy.onrender.com/assets/gastro.jpg' }
     ]
   },
   '3': {
     nom: 'Chez Manon',
     menu: [
-      { id: 'express-3', nom: 'Menu Express', prix: '1500 FCFA', description: 'Sandwich ou salade + boisson' },
-      { id: 'dej-3', nom: 'Menu Déjeuner', prix: '3000 FCFA', description: 'Plat traditionnel + dessert' },
-      { id: 'gastro-3', nom: 'Menu Gastronomique', prix: '8000 FCFA', description: 'Menu 3 services avec vin' }
+      { id: 'express-3', nom: 'Menu Express', prix: '1500 FCFA', description: 'Sandwich ou salade + boisson', image: 'https://resto-back-xazy.onrender.com/assets/express.jpg' },
+      { id: 'dej-3', nom: 'Menu Déjeuner', prix: '3000 FCFA', description: 'Plat traditionnel + dessert', image: 'https://resto-back-xazy.onrender.com/assets/dej2.jpg' },
+      { id: 'gastro-3', nom: 'Menu Gastronomique', prix: '8000 FCFA', description: 'Menu 3 services avec vin', image: 'https://resto-back-xazy.onrender.com/assets/gastro.jpg' }
     ]
   }
 };
@@ -52,7 +52,7 @@ const createMenusGrid = (restaurantId) => {
       menu.id,
       menu.nom,
       description,
-      'menu-icon.png',
+      menu.image,
       {
         type: 'GET',
         // Ici tu peux mettre l'URL pour voir les détails du menu spécifique
@@ -61,74 +61,7 @@ const createMenusGrid = (restaurantId) => {
     );
   });
 
-  // ✅ CORRECTION : Action de retour avec redirection
-  // grid.addAction(
-  //   'retour-restaurant',
-  //   '↩️ Retour au restaurant',
-  //   'Retourner aux détails du restaurant',
-  //   'back-icon.png',
-  //   {
-  //     type: 'GET',
-  //     href: `https://resto-back-xazy.onrender.com/mobile/restaurents/${restaurantId}`
-  //   }
-  // );
-
   return grid.toJSON();
 };
 
 module.exports = { createMenusGrid };
-
-
-
-
-
-
-// const { ReaderView } = require('@numerum-tech/cmsdk');
-
-// // Données des menus pour chaque restaurant
-// const menusData = {
-//   '1': {
-//     nom: 'Chez Nana',
-//     menus: [
-//       'Menu Petit-déjeuner - 2000 FCFA',
-//       'Menu Déjeuner - 3500 FCFA',
-//       'Menu Dîner - 5000 FCFA'
-//     ]
-//   },
-//   '2': {
-//     nom: 'Chez Bordille',
-//     menus: [
-//       'Menu Brunch - 2500 FCFA',
-//       'Menu Midi - 4000 FCFA',
-//       'Menu Soir - 6000 FCFA',
-//       'Menu Famille (4 pers) - 15000 FCFA'
-//     ]
-//   },
-//   '3': {
-//     nom: 'Chez Manon',
-//     menus: [
-//       'Menu Express - 1500 FCFA',
-//       'Menu Déjeuner - 3000 FCFA',
-//       'Menu Gastronomique - 8000 FCFA'
-//     ]
-//   }
-// };
-
-// // Fonction pour créer la vue des menus selon l'ID du restaurant
-// const createMenusReader = (restaurantId) => {
-//   const data = menusData[restaurantId];
-
-//   if (!data) {
-//     return null;  // Restaurant non trouvé
-//   }
-
-//   return new ReaderView(
-//     `menus-restaurant-${restaurantId}`,
-//     `Menus - ${data.nom}`
-//   )
-//     .setIntro('Liste des menus disponibles')
-//     .addListField(data.menus)
-//     .setPrev(`https://resto-back-xazy.onrender.com/mobile/restaurents/${restaurantId}`);
-// };
-
-// module.exports = { createMenusReader };
