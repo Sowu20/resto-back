811e8b08e4ba8d8d80d24645dbbe5e44af84ce9b
Sowu20 <sowukelly67@gmail.com>
2026-01-29 10:40:48 +0000

    Coorection des endpoint de Restaurent

--- routes/restaurentRoute.js
+++ routes/restaurentRoute.js
@@ -8,13 +8,13 @@ const check = require('../middlewares/checkrestaurentmiddleware');
 
 router.post('/restaurent', auth, role(['Restaurateur']), restaurentController.createResto);
 router.get('/restaurent', restaurentController.listResto);
-router.get('/restaurent/:id', restaurentController.detailResto);
-router.get('/restaurent/localisation', restaurentController.getRestaurentsLoc);
-router.put('/restaurent/:id', auth, check, restaurentController.updateResto);
-router.delete('/restaurent/:id', auth, role(['Admin']), restaurentController.deleteResto);
 router.get('/restaurent/stats', auth, restaurentController.getRestaurentStats);
 router.get('/restaurent/liste', auth, restaurentController.getRestaurents);
 router.get('/restaurent/search', auth, restaurentController.searchRestaurent);
-router.get('/restaurent', auth, restaurentController.getStatusRestaurents);
+router.get('/restaurent/status_restaurent', auth, restaurentController.getStatusRestaurents);
+router.get('/restaurent/localisation', restaurentController.getRestaurentsLoc);
+router.get('/restaurent/:id', auth, restaurentController.detailResto);
+router.put('/restaurent/:id', auth, check, restaurentController.updateResto);
+router.delete('/restaurent/:id', auth, role(['Admin']), restaurentController.deleteResto);
 
 module.exports = router;
