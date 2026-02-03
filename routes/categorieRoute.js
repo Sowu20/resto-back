b0eca0c2f88a5340cf4b88df22d1f8a49d248a28
Sowu20 <sowukelly67@gmail.com>
2026-02-03 15:55:31 +0000

    Ajout d'endpoint

--- routes/categorieRoute.js
+++ routes/categorieRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const categorieController = require('../controllers/categorieController');
 const auth = require('../middlewares/authmiddleware');
+const role = require('../middlewares/rolemiddleware');
 
 router.post('/categorie', auth, categorieController.createCategorie);
 router.get('/categorie', auth, categorieController.listCategorie);
 router.get('/categorie/:id', auth, categorieController.detailCategorie);
+router.get('/categorie/menu/:menuId/repas', auth, role(['restaurent', 'Admin']), categorieController.getCategorieMenu);
 router.put('/categorie/:id', auth, categorieController.updateCategorie);
 router.delete('/categorie/:id', auth, categorieController.deleteCategorie);
 
