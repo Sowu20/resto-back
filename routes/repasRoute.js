ee6e1af94f3e4ce7b855bb28a2ddc17048c24ac5
Sowu20 <sowukelly67@gmail.com>
2026-02-03 15:55:46 +0000

    Ajout d'endpoint

--- routes/repasRoute.js
+++ routes/repasRoute.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const repasController = require('../controllers/repasController');
 const auth = require('../middlewares/authmiddleware');
+const role = require('../middlewares/rolemiddleware');
 
 router.post('/repas', auth, repasController.createRepas);
 router.get('/repas', auth, repasController.listRepas);
 router.get('/repas/:id', auth, repasController.detailRepas);
 router.put('/repas/:id', auth, repasController.updateRepas);
 router.delete('/repas/:id', auth, repasController.deleteMenu);
-
+router.get('/repas/categorie/:categorieId/repas', auth, role(['restaurant','Admin']), repasController.getRepasByCategorie);
 module.exports = router;
