95cd89c03e182d748d76b7a77498510192653e27
Sowu20 <sowukelly67@gmail.com>
2026-02-05 14:25:45 +0000

    Création des endpoint pour la gestion des promotion

--- routes/promotionRoute.js
+++ routes/promotionRoute.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const promotionController = require('../controllers/promotionController');
+const auth = require('../middlewares/authmiddleware');
+const role = require('../middlewares/rolemiddleware');
 
-router.post('/promotion');
-router.get('/promotion');
-router.get('/promotion/:id');
-router.get('/promotion/:id');
-router.delete('/promotion/:id');
+router.post('/promotion', auth, role(['Admin', 'restaurent']), promotionController.createPromotion);
+router.get('/promotion', auth, role(['Admin', 'restaurent']), promotionController.listPromotion);
+router.get('/promotion/actives', auth, role(['Admin', 'restaurent']), promotionController.avaiblePromotion);
+router.get('/promotion/expires', auth, role(['Admin', 'restaurent']), promotionController.duePromotion);
+router.get('/promotion/:id', auth, role(['Admin', 'restaurent']), promotionController.detailPromotion);
+router.get('/promotion/restaurent/:restaurentId', auth, role(['Admin', 'restaurent']), promotionController.promotionByRestaurent);
+router.put('/promotion/:id', auth, role(['Admin', 'restaurent']), promotionController.updatePromotion);
+router.delete('/promotion/:id', auth, role(['Admin', 'restaurent']), promotionController.deletePromotion);
 
 module.exports = router;
