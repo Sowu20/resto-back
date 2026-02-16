05b183e174faa61457abad84705ea8d7cfcc9011
Sowu20 <sowukelly67@gmail.com>
2026-02-16 16:47:17 +0000

    Correction des routes

--- routes/restaurent/tableRoute.js
+++ routes/restaurent/tableRoute.js
@@ -6,10 +6,10 @@ const auth = require('../../middlewares/authmiddleware');
 const role = require('../../middlewares/rolemiddleware');
 const check = require('../../middlewares/checkrestaurentmiddleware');
 
-router.post('/table', auth, role(['Restaurateur']), check, tableController.createTable);
-router.get('/table', auth, role(['Restaurateur']), tableController.listTable);
-router.get('/table/:id', auth, role(['Restaurateur']), tableController.detailTable);
-router.put('/table/:id', auth, role(['Restaurateur']), check, tableController.updateTable);
+router.post('/table', auth, role(['Admin', 'restaurent']), check, tableController.createTable);
+router.get('/table', auth, role(['Admin', 'restaurent']), tableController.listTable);
+router.get('/table/:id', auth, role(['Admin', 'restaurent']), tableController.detailTable);
+router.put('/table/:id', auth, role(['Admin', 'restaurent']), check, tableController.updateTable);
 router.delete('/table/:id', auth, check, tableController.deleteTable);
 router.get('/table/scan/:qrCode', tableController.scanQrCode);
 router.get('/table/menu/:qrCode/', tableController.getMenuandMeal);
