a0f4a0f25f29f52bdf2ff6221538f2eb9619a20e
Sowu20 <sowukelly67@gmail.com>
2026-02-16 11:38:23 +0000

    Correction des endpoints

--- routes/commandeRoute.js
+++ routes/commandeRoute.js
@@ -7,13 +7,13 @@ const role = require('../middlewares/rolemiddleware');
 
 router.post('/commande', auth, commandeController.createCommande);
 router.post('/commande', commandeController.faireCommande);
-router.get('/commande', auth, commandeController.listCommande);
 router.get('/commande/admin/status', auth, role(['Admin']), admincommandeController.CommandesStats);
 router.get('/commande/admin/statorder', auth, role(['Admin']), admincommandeController.StatOrders);
 router.get('/commande/admin/listcommande', auth, role(['Admin']), admincommandeController.listCommande);
 router.get('/commande/admin/revenuchart', auth, role(['Admin']), admincommandeController.RevenuChart);
 router.get('/commande/admin/recent_order', auth, role(['Admin']), admincommandeController.RecentOrders);
-router.get('/commande/admin/top_sell', auth, role(['Admin']), admincommandeController.topSellingMeals)
+router.get('/commande/admin/top_sell', auth, role(['Admin']), admincommandeController.topSellingMeals);
+router.get('/commande/:restaurentId', auth, commandeController.listCommande);
 router.get('/commande/:id', auth, commandeController.detailCommande);
 router.put('/commande/:id', auth, commandeController.updateCommande);
 router.delete('/commande/:id', auth, commandeController.deleteCommande);
