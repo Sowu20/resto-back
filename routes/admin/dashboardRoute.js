237f064d5febd13f046119b932d5c071d3a747f7
Sowu20 <sowukelly67@gmail.com>
2026-02-17 17:20:42 +0000

    Correction des routes

--- routes/admin/dashboardRoute.js
+++ routes/admin/dashboardRoute.js
@@ -4,11 +4,9 @@ const dashboardController = require('../../controllers/admin/dashboardController
 const auth = require('../../middlewares/authmiddleware');
 const role = require('../../middlewares/rolemiddleware');
 
-router.get('/dashboard/admin/status', auth, role(['Admin']), dashboardController.CommandesStats);
-router.get('/dashboard/admin/statorder', auth, role(['Admin']), dashboardController.StatOrders);
-router.get('/dashboard/admin/listcommande', auth, role(['Admin']), dashboardController.listCommande);
-router.get('/dashboard/admin/revenuchart', auth, role(['Admin']), dashboardController.RevenuChart);
-router.get('/dashboard/admin/recent_order', auth, role(['Admin']), dashboardController.RecentOrders);
-router.get('/dashboard/admin/top_sell', auth, role(['Admin']), dashboardController.topSellingMeals);
+router.get('/admin/dashboard/statorder', auth, role(['Admin']), dashboardController.StatsOrders);
+router.get('/admin/dashboard/revenuchart', auth, role(['Admin']), dashboardController.RevenueChart);
+router.get('/admin/dashboard/recent_order', auth, role(['Admin']), dashboardController.RecentOrders);
+router.get('/admin/dashboard/top_sell', auth, role(['Admin']), dashboardController.TopSellers);
 
 module.exports = router;
