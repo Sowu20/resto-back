6a5c6dcafcab915f7db51f6afdd44a62ddfd2014
Sowu20 <sowukelly67@gmail.com>
2026-02-19 09:18:24 +0000

    Endpoint

--- routes/admin/dashboardRoute.js
+++ routes/admin/dashboardRoute.js
@@ -8,5 +8,6 @@ router.get('/admin/dashboard/statorder', auth, role(['Admin']), dashboardControl
 router.get('/admin/dashboard/revenuchart', auth, role(['Admin']), dashboardController.RevenueChart);
 router.get('/admin/dashboard/recent_order', auth, role(['Admin']), dashboardController.RecentOrders);
 router.get('/admin/dashboard/top_sell', auth, role(['Admin']), dashboardController.TopSellers);
+router.get('/admin/dashboard/stat_status', auth, role(['Admin']), dashboardController.StatsStaus);
 
 module.exports = router;
