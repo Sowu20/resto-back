a45e934e2da9693c73153f7b2f2bab6d6ae7aa58
Sowu20 <sowukelly67@gmail.com>
2026-02-24 11:03:03 +0000

    Correction des endpoints

--- routes/restaurent/menuRoute.js
+++ routes/restaurent/menuRoute.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const menuController = require('../../controllers/restaurent/menuController');
 const auth = require('../../middlewares/authmiddleware');
+const upload = require('../../middlewares/upload');
 
-router.post('/menu', auth, menuController.createMenu);
-router.get('/menu', auth, menuController.listMenu);
-router.get('/menu/:id', auth, menuController.detailMenu);
-router.put('/menu/:id', auth, menuController.updateMenu);
-router.delete('/menu/:id', auth, menuController.deleteMenu);
+router.post('/menu/:restaurentId', auth, upload.single('image'), menuController.createMenu);
+router.get('/menu/:restaurentId', auth, menuController.listMenu);
+router.get('/menu/:restaurentId/:id', auth, menuController.detailMenu);
+router.put('/menu/:restaurentId/:id', auth, menuController.updateMenu);
+router.delete('/menu/:restaurentId/:id', auth, menuController.deleteMenu);
 
 module.exports = router;
