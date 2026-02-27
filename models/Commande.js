4c2a5e24e56244698ff968d053be12212f9836b2
Julio DAKLU <sergiodaklu12@gmail.com>
2026-02-27 14:05:32 +0000

    Configuration sur mode Commande et About Zamoral

--- models/Commande.js
+++ models/Commande.js
@@ -33,7 +33,7 @@ const cmdeSchema = new mongoose.Schema({
             total: {
                 type: Number,
                 required: true
-            } 
+            }
         }
     ],
     status: {
@@ -46,7 +46,7 @@ const cmdeSchema = new mongoose.Schema({
     },
     payment_method: {
         type: String,
-        enum: ['espece', 'virement']
+        enum: ['espece', 'virement', 'tmoney', 'flooz']
     },
     source: {
         type: String
