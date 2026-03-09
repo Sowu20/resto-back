9aaffc37b300120417a907ff1ac0e754da7c5379
Julio DAKLU <sergiodaklu12@gmail.com>
2026-03-09 16:21:54 +0000

    correction de mots

--- sdk/views/menuView/mainMenu.js
+++ sdk/views/menuView/mainMenu.js
@@ -1,7 +1,7 @@
 const { ActionListView } = require('@numerum-tech/cmsdk');
 
 const mainMenu = new ActionListView('main-menu', 'Acceuil')
-    .addAction('restaurents', 'Restaurants', 'Consulter la liste des restaurants disponibles', '/restaurents')
+    .addAction('restaurents', 'Restaurants', 'Consulter la liste des restaurants disponible', '/restaurents')
     .addAction('scan-qr', 'Scanner QR', 'Scanner un code QR', '/scan-qr')
     .addAction('about', 'À propos', 'En savoir plus sur l\'application', '/about');
 
