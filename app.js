506eaa78b9aa8e2d0f31f8e8c72383f861e634d2
Julio DAKLU <sergiodaklu12@gmail.com>
2026-03-09 15:53:04 +0000

    correction de mots

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-  const fs = require('fs');
-  fs.appendFileSync('server-debug.log', `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}\n`);
-  next();
-});
-
 app.use(express.json());
 
 app.use(cors({
--- sdk/views/menuView/mainMenu.js
+++ sdk/views/menuView/mainMenu.js
@@ -1,7 +1,7 @@
 const { ActionListView } = require('@numerum-tech/cmsdk');
 
 const mainMenu = new ActionListView('main-menu', 'Acceuil')
-    .addAction('restaurents', 'Restaurents', 'Consulter la liste des restaurents disponible', '/restaurents')
+    .addAction('restaurants', 'Restaurants', 'Consulter la liste des restaurants disponibles', '/restaurents')
     .addAction('scan-qr', 'Scanner QR', 'Scanner un code QR', '/scan-qr')
     .addAction('about', 'À propos', 'En savoir plus sur l\'application', '/about');
 
