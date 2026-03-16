94c2eb05be6d5a3a8921b2e32bf51e9986365627
Sowu20 <sowukelly67@gmail.com>
2026-03-16 09:10:18 +0000

    Ajout d'endpoint

--- routes/restaurent/notificationRoute.js
+++ routes/restaurent/notificationRoute.js
@@ -6,10 +6,12 @@ const auth = require('../../middlewares/authmiddleware');
 router.post('/notification', auth, notificationController.createNotification);
 router.get('/notification', auth, notificationController.listNotifications);
 router.get('/notification/stats', auth, notificationController.statsNotification);
+router.get('/notification/settings/:restaurentId', auth, notificationController.settings);
 router.get('/notification/:userId', auth, notificationController.listNotificationUser);
 router.get('/notification/count/:userId', auth, notificationController.unReadCount);
 router.get('/notification/:id', auth, notificationController.detailNotification);
 router.put('/notification/read/:notificationId', auth, notificationController.markAsRead);
+router.put('/notification/settings/:restaurentId', auth, notificationController.updateSettings);
 router.put('/notification/:id', auth, notificationController.updateNotification);
 router.delete('/notification/:id', auth, notificationController.deleteNotification);
 
