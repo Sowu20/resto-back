655dc7804cebe7e4a48654b79c1f53339997c64c
Sowu20 <sowukelly67@gmail.com>
2026-03-17 16:58:09 +0000

    Correction

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.enable('trust proxy');
 app.use(express.json());
 
 app.use(cors({
-  origin: "*",
+  origin: true,
   methods: "*",
   // allowedHeaders: ['Content-Type', 'Authorization'],
   // credentials: true
